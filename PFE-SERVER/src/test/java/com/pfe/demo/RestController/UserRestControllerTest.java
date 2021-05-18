81e378cf275d1a23095d70a59804a52f45d9326c
farah-chourou <78874349+farah-chourou@users.noreply.github.com>
2021-05-18 12:12:14 +0100

    18/05/2021

---
+++ PFE-SERVER/src/test/java/com/pfe/demo/RestController/UserRestControllerTest.java
@@ -0,0 +1,13 @@
+package com.pfe.demo.RestController;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@WebMvcTest(UserRestController.class)
+class UserRestControllerTest {
+
+    @Test
+    void getAllResponsable() {
+    }
+}
